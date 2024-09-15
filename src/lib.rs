847145ff8e9af645102502f45c86161cf1ef6005
kofta999 <mostafaxxx555@gmail.com>
2024-09-15 14:57:16 +0300

    Add -w and -l arguments

--- src/lib.rs
+++ src/lib.rs
@@ -23,6 +23,8 @@ impl Config {
         args.for_each(|arg| {
             match arg.as_str() {
                 "-c" => config.bytes_arg = true,
+                "-l" => config.lines_arg = true,
+                "-w" => config.words_arg = true,
                 x => config.file_path = x.to_string(),
             };
         });
@@ -43,18 +45,38 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     // should do something else when im using stdin
 
     if config.bytes_arg {
-        let chars_count = count_chars(&file);
+        let chars_count = count_bytes(&file);
         res.push(chars_count.to_string());
     };
+
+    if config.lines_arg {
+        let lines_count = count_lines(&file);
+        res.push(lines_count.to_string())
+    }
+
+    if config.words_arg {
+        let words_count = count_words(&file);
+        res.push(words_count.to_string())
+    }
 
     res.push(filename);
     println!("{}", res.join(" "));
     Ok(())
 }
 
-fn count_chars(content: &str) -> usize {
+fn count_bytes(content: &str) -> usize {
     // len() counts the number of bytes
     content.len()
+}
+
+fn count_lines(content: &str) -> usize {
+    content.lines().count()
+}
+
+fn count_words(content: &str) -> usize {
+    content
+        .lines()
+        .fold(0, |acc, line| acc + line.trim().split_whitespace().count())
 }
 
 #[cfg(test)]
@@ -62,11 +84,46 @@ mod tests {
     use super::*;
 
     #[test]
-    fn count_bytes() {
+    fn test_count_bytes() {
         let s = "Hello world!";
-        assert_eq!(12, count_chars(s));
+        assert_eq!(12, count_bytes(s));
 
         let s = "美味しいご飯だった";
-        assert_eq!(27, count_chars(s));
+        assert_eq!(27, count_bytes(s));
+    }
+
+    #[test]
+    fn test_count_lines() {
+        let s = "\
+Hello world!
+rust is cool
+but typescript is cooler
+";
+
+        assert_eq!(3, count_lines(s));
+    }
+
+    #[test]
+    fn test_count_words() {
+        let s = "Title: The Art of War";
+        assert_eq!(5, count_words(s));
+
+        let s = "\
+            The Project Gutenberg eBook of The Art of War
+
+            This ebook is for the use of anyone anywhere in the United States and
+            most other parts of the world at no cost and with almost no restrictions
+            whatsoever. You may copy it, give it away or re-use it under the terms
+            of the Project Gutenberg License included with this ebook or online
+            at www.gutenberg.org. If you are not located in the United States,
+            you will have to check the laws of the country where you are located
+            before using this eBook.
+
+            Title: The Art of War
+
+
+            Author: active 6th century B.C. Sunzi
+";
+        assert_eq!(102, count_words(s));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,6 @@ fn main() {
         process::exit(1);
     });
 
-    println!("{config:?}");
-
     run(config).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
